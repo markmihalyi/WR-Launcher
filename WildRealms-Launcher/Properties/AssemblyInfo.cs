634409ddbaf96d23560e262c2db0a0906ec2a6ee
Markio <nlisteners@gmail.com>
2020-01-18 13:39:18 +0100

    asd

--- WildRealms-Launcher/Properties/AssemblyInfo.cs
+++ WildRealms-Launcher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WildRealms_Launcher")]
+[assembly: AssemblyTitle("WildRealms-Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WildRealms_Launcher")]
+[assembly: AssemblyProduct("WildRealms-Launcher")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("204d9d18-d7d7-44c9-8380-f4cb6c8784be")]
+[assembly: Guid("69ff6b76-69ee-4fdf-9d28-fb2f3caff80f")]
 
 // Version information for an assembly consists of the following four values:
 //
